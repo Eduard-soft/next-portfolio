587ce063cd12d3fd58bdfbfda64d4a5ec687d8bb
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2023-07-06 22:58:29 +0300

    create portfolio pages

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,29 +1,44 @@
 import styles from './page.module.css'
 import Button from '@/components/button/Button'
-import Image from 'next/image';
+import Image from 'next/image'
+import { items } from './data'
+import { notFound } from 'next/navigation'
+
+const getData = (cat) => {
+	const data = items[cat]
+
+	if(data) {
+		return data
+	}
+	return notFound()
+}
 
 const Category = ({params}) => {
-	console.log(params);
+	//console.log(params)
+	const data = getData(params.category)
 	return (
 		<div className={styles.container}>
 		<h1 className={styles.catTitle}>{params.category}</h1>
-		<div className={styles.item}>
-			<div className={styles.content}>
-				<h1 className={styles.title}>Title</h1>
-				<p className={styles.desc}>Description</p>
-				<Button text="See More" url="#" />
-			</div>
-			<div className={styles.imgContainer}>
-				<Image
-					className={styles.img}
-					fill={true}
-					src="https://img.freepik.com/free-photo/paper-people-chain-on-green-grass-unity-concept_632805-35.jpg?w=1380&t=st=1685890833~exp=1685891433~hmac=46f15c2161ca525e238313ec9dc0812864411c64035b664d7abecd75e33494e1"
-					 alt="Title"
-				/>
-			</div>
-		</div>
+
+		{data.map((item) => (
+					<div className={styles.item} key={item.id}>
+					<div className={styles.content}>
+						<h1 className={styles.title}>{item.title}</h1>
+						<p className={styles.desc}>{item.desc}</p>
+						<Button text="See More" url="#" />
+					</div>
+					<div className={styles.imgContainer}>
+						<Image
+							className={styles.img}
+							fill={true}
+							src={item.image}
+								alt={item.title}
+						/>
+					</div>
+				</div>
+		))}
 	</div>
-	);
+	)
 }
 
 export default Category;
